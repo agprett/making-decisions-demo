a087d47364488df2bbc411900a115aa6faff337d
Alec <prettale4@gmail.com>
2021-12-15 12:34:39 -0700

    finished vars & cons demo

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -4,3 +4,52 @@
     Jamie Lannister.
 */
 
+let jonSnowAttack = 25;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+let jamieLannisterAttack = 55;
+
+if(jonSnowAttack > jamieLannisterAttack){
+  console.log('Jon has a better attack than Jamie')
+} else if(jamieLannisterAttack > jonSnowAttack) {
+  console.log('Jamie has a better attack than Jon')
+} else {
+  console.log('Jon and Jamie have the same atack')
+}
+
+jonSnowDefense += 15
+
+if(jonSnowHealth <= jamieLannisterAttack - jonSnowDefense){
+  console.log('Jon has been slain')
+} else {
+  //jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+  jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+  console.log(`Jon's health is now ${jonSnowHealth}`)
+}
+
+if((jonSnowHealth + 50) > 100){
+  jonSnowHealth = 100
+} else {
+  jonSnowHealth += 50
+}
+
+// while(jonSnowHealth > 0){
+//   jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+//   console.log(`Jon's health is ${jonSnowHealth}`)
+//   if(jonSnowHealth <= 0){
+//     console.log(`Jon has been slain`)
+//   }
+// }
+
+for(let i = 0; i < 5; i++){
+  // if(i < 5){
+  if(jonSnowHealth <= 0){
+    console.log('Jon was slain')
+  } else {
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+    console.log(`Jon Snow's health is ${jonSnowHealth}`)
+  }
+  // }
+  // i++
+}
